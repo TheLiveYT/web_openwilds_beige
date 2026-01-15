cbe33b208c5c00dc39d91a0d326feb7bd1d0f874
TheLiveYT <daniel.vanekjr@gmail.com>
2026-01-15 21:47:20 +0100

    first commit

---
+++ loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").classList.add("hide");
+        document.getElementById("content").classList.add("show");
+    }, 3400);
+});
---
+++ toast.js
@@ -0,0 +1,13 @@
+function showToast(message, duration = 3000) {
+  const container = document.getElementById("toast-container");
+
+  const toast = document.createElement("div");
+  toast.className = "toast";
+  toast.textContent = message;
+
+  container.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, duration);
+}
